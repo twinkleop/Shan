649d2a6a3f5db64da217d0ec84e2acfb562e1feb
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-10 19:34:59 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 @Client.on_message(command("help") & filters.private & ~filters.group & ~filters.edited)
 async def help_cmd(client: Client, message: Message):
-    await message.reply_sticker("CAACAgIAAx0CZD3aQwACJDFjJ-eam0HLnauJcFZ2QG1VMhVI5AAC3QgAArNsUEnxuIcx41T1ECkE")
+    await message.reply_sticker("CAACAgUAAx0CZ80CewABAn7iY0QmDt7nsecHlSDH7oMXmtavNH4AAhMHAALnnCFW1zONdkHrocAeBA")
     await message.reply_photo(f"{START_IMG}", caption=f"""
 🔴 **AVAILABLE COMMAND IN {BOT_NAME} :**
 
