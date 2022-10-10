a668cb4028e317b106daf172dcfa8f2eeb331cbe
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-10 19:08:58 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -207,10 +207,11 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton("📨 Support", url=f"https://t.me/{SUPPORT_GROUP}"),
-                    InlineKeyboardButton("📨 Channel", url=f"https://t.me/{CHANNEL_UPDATES}")
+                    InlineKeyboardButton("📨 Updates", url=f"https://t.me/TheQueenUpdates")
                 ],
                 [
-                   InlineKeyboardButton("🗑️ Close Menu", callback_data="close_play")
+                   InlineKeyboardButton("🗑️ Close Menu", callback_data="close_play"),
+                   InlinekeyboardButton("👤 Powered by", https://t.me/QUEEN_NETWORK)
                 ],
             ]
         ),
